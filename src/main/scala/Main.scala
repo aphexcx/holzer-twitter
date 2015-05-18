c3e7a1e9650f038b2d3a6856b9aa720939568313
Afik Cohen <aphex@aphex.cx>
2015-05-17 17:32:38 -0700

    Auto-respond to mentions of "jenny holzer" with a truism.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,8 +9,9 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 
 import akka.actor.ActorSystem
-import dispatch._
-import twitter.response.Tweet
+import dispatch.{Http, _}
+import twitter.response
+import twitter.response.{Tweet, User}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -30,54 +31,130 @@ object Main extends App {
 
   def R(s: String): String = getClass.getResource(s).getPath
 
-  if (!Files.exists(Paths.get("used.bin")))
-    Files.createFile(Paths.get("used.bin"))
+  case class Recorder(filename: String) {
 
-  def load(filename: String): Set[String] = {
-    val ab: Array[Byte] = Files.readAllBytes(Paths.get(filename))
-    if (ab.length == 0)
-      Set.empty[String]
-    else
-      BinaryPickle(ab).unpickle[Set[String]]
-  }
+    if (!Files.exists(Paths.get(filename)))
+      Files.createFile(Paths.get(filename))
 
-  def quote(lines: Seq[String], used: Set[String]): Option[String] =
-    Random.shuffle(lines filterNot used).headOption
-
-  def dump(xs: Set[String], filename: String = "used.bin"): Unit = {
-    Files.write(Paths.get(filename), xs.pickle.value)
-  }
-
-  def use(s: String, used: Set[String]): Set[String] =
-    used + s.trim
-
-  def postWithRollover(quotes: Seq[String], used: Set[String],
-                       effect: (String, Set[String]) => Unit): Unit =
-    quote(quotes, used).map(q => effect(q, used)) orElse {
-      val fresh = Set.empty[String]
-      quote(quotes, fresh).map(q => effect(q, fresh))
+    def dump(xs: Set[String]): Unit = {
+      Files.write(Paths.get(filename), xs.pickle.value)
     }
 
-  def postTweet(q: String, used: Set[String]): Unit = {
-    val f: Future[Tweet] = http(client(Status.update(q)) OK as.repatch.twitter.response.Tweet)
-    f onComplete {
-      case Success(t) => dump(use(q, used)); println(t)
+    def load: Set[String] = {
+      val ab: Array[Byte] = Files.readAllBytes(Paths.get(filename))
+      if (ab.length == 0)
+        Set.empty[String]
+      else
+        BinaryPickle(ab).unpickle[Set[String]]
+    }
+
+    def use(s: String, used: Set[String]): Set[String] =
+      used + s.trim
+
+    def used: Set[String] = load
+
+    def markUsed(s: String) = dump(use(s, used))
+
+    def reset() = dump(Set.empty[String])
+
+  }
+
+  def unusedQuote(quotes: Seq[String], used: Set[String]): Option[String] =
+    Random.shuffle(quotes filterNot used).headOption
+
+
+  def getAnUnusedQuote(quotes: Seq[String], recorder: Recorder): String =
+    unusedQuote(quotes, recorder.used).getOrElse {
+      recorder.reset()
+      unusedQuote(quotes, recorder.used).get
+    }
+
+  //  def postWithRollover(quotes: Seq[String], recorder: Recorder,
+  //                       effect: (String, Recorder) => Unit): Unit =
+  //    getAnUnusedQuote(quotes, recorder)
+  //    unusedQuote(quotes, recorder.used).map(q => effect(q, recorder)) orElse {
+  ////      val fresh = Set.empty[String]
+  //      recorder.reset()
+  //      unusedQuote(quotes, recorder.used).map(q => effect(q, recorder))
+  //    }
+
+  // need to post tweet and record the tweet used.
+  //  def postTweet(q: String)(onSuccess: (Set[String], String) => Unit): Unit = {
+  //    val f: Future[Tweet] = http(client(Status.update(q)) OK as.repatch.twitter.response.Tweet)
+  //    f onComplete {
+  //      case Success(t) => dump(use(q, used)); println(t)
+  //      case Failure(e) => println("Error posting tweet: " + e.getMessage)
+  //    }
+  //  }
+
+  def postTweet(q: String): Future[Tweet] = {
+    http(client(Status.update(q)) OK as.repatch.twitter.response.Tweet)
+  }
+
+  def postTweetAndMarkUsed(s: String, recorder: Recorder): Unit = {
+    postTweet(s) onComplete {
+      case Success(t) => recorder.markUsed(s); println(t)
       case Failure(e) => println("Error posting tweet: " + e.getMessage)
     }
   }
 
-  def tweetAQuote(): Unit =
-  // tweet out a truism.
-  // if successful, mark the quote as used and print the tweet.
-  // if failed, print the exception.
-    postWithRollover(quotes, load("used.bin"), postTweet)
+  def tweetAQuote(): Unit = {
+    // tweet out a truism.
+    // if successful, mark the quote as used and print the tweet.
+    // if failed, print the exception.
+    val recorder = Recorder("used.bin")
+    postTweetAndMarkUsed(getAnUnusedQuote(quotes, recorder), recorder)
+  }
+
+  //    postTweet(unusedQuote(quotes),) onComplete {
+  //      case Success(t) => Recorder("used.bin").markUsed(s); println(t)
+  //      case Failure(e) => println("Error posting tweet: " + e.getMessage)
+  //    }
+  //, dump(filename = "used.bin"), postTweetAndMarkUsed)
 
 
   // future: if i want to tweet at specific times of day
   // https://github.com/theatrus/akka-quartz
   //  val quartzActor = system.actorOf(Props[QuartzActor])
 
+  def respondToTweets(tweets: List[Tweet]): Unit =
+    tweets foreach respondToTweet
+
+
+  def respondToTweet(t: Tweet): Unit = {
+    val respondedTweets = Recorder("respondedTweetIDs.bin")
+
+    if (!respondedTweets.used.contains(t.id.toString())) {
+      t.user foreach { user =>
+        val q = getAnUnusedQuote(quotes, Recorder("used.bin"))
+        replyToUser(user, q) onComplete {
+          case Success(postedTweet) => respondedTweets.markUsed(t.id.toString()); println(postedTweet)
+          case Failure(e) => println("Error posting tweet: " + e.getMessage)
+
+        }
+      }
+    }
+
+    def replyToUser(user: User, s: String): Future[Tweet] =
+      postTweet("@" + user + " " + s)
+  }
+
+  def searchForJenny: Future[response.Search] =
+    http(client(Search("jenny holzer")) OK as.repatch.twitter.response.Search)
+
+
+  def searchAndRespond(): Unit =
+    searchForJenny onComplete {
+      case Success(s) => respondToTweets(s.statuses)
+      case Failure(e) => println("Error when searching: " + e.getMessage)
+    }
+
+
   system.scheduler.schedule(0 seconds, 10.5 hours) {
     tweetAQuote()
+  }
+
+  system.scheduler.schedule(0 seconds, 120 seconds) {
+    searchAndRespond()
   }
 }
--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,58 +1,57 @@
 import java.io.File
-import java.nio.file.NoSuchFileException
 
 import Main._
 import org.scalatest.{FunSpec, Matchers}
 
+
 class MainSpec extends FunSpec with Matchers {
-  describe("use") {
-    describe("given a string and a set") {
-      it("returns a new set containing the string and the previous set") {
-        val s = "hello"
-        val set = Set[String]("goodbye", "farewell")
-        val set2 = Set[String]("hello", "goodbye", "farewell")
-        use(s, set) shouldBe set2
-      }
-    }
-  }
-
-  describe("dump and load") {
-    it("writes a given set to a file and reads it back") {
-      val set = Set[String]("hi", "bye")
-      val path = File.createTempFile("mainspec", "dump").getPath
-      dump(set, path)
-      load(path) shouldBe set
-    }
-
-    it("throws an exception if the file to load from doesn't exist") {
-      an[NoSuchFileException] shouldBe thrownBy(load("bogus"))
-    }
-  }
-
-  describe("tweeting quotes") {
-    it("rolls over correctly when all the quotes are used up") {
-      val quotes: Seq[String] = Seq("a", "b", "c")
-      val used: Set[String] = Set("a", "b", "c")
-
-      class effectSpy {
-        var timesCalled: Int = 0
-        var setReceieved: Set[String] = Set("~didn't see anything~")
-
-        def effectSpy(q: String, used: Set[String]): Unit = {
-          timesCalled += 1
-          setReceieved = used
+  describe("Recorder") {
+    val path = File.createTempFile("mainspec", "dump").getPath
+    val recorder = Recorder(path)
+    describe("use") {
+      describe("given a string and a set") {
+        it("returns a new set containing the string and the previous set") {
+          val s = "hello"
+          val set = Set[String]("goodbye", "farewell")
+          val set2 = Set[String]("hello", "goodbye", "farewell")
+          recorder.use(s, set) shouldBe set2
         }
       }
+    }
 
-      val spy = new effectSpy
+    describe("dump and load") {
+      it("writes a given set to a file and reads it back") {
+        val set = Set[String]("hi", "bye")
 
-      postWithRollover(quotes, used, spy.effectSpy)
+        recorder.dump(set)
+        recorder.load shouldBe set
+      }
 
-      spy.setReceieved shouldBe Set.empty[String]
-      spy.timesCalled shouldBe 1
+      //      it("throws an exception if the file to load from doesn't exist") {
+      //        an[NoSuchFileException] shouldBe thrownBy(Recorder("bogus"))
+      //      }
     }
   }
 
+  describe("unused quote rollover") {
+    val path = File.createTempFile("mainspec", "dump").getPath
+    val recorder = Recorder(path)
 
+    val quotes: Seq[String] = Seq("a", "b", "c")
+
+    it("returns an unused quote when there's one available") {
+      getAnUnusedQuote(quotes, recorder) should (be("a") or be("b") or be("c"))
+    }
+
+    it("rolls over correctly when all the quotes are used up") {
+      recorder.markUsed("a")
+      getAnUnusedQuote(quotes, recorder) should (be("b") or be("c"))
+      recorder.markUsed("b")
+      getAnUnusedQuote(quotes, recorder) should be("c")
+      recorder.markUsed("c")
+      getAnUnusedQuote(quotes, recorder) should (be("a") or be("b") or be("c"))
+
+    }
+  }
 
 }
